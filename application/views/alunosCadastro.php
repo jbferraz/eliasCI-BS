3ee78890a8d7e17c655930d247e891b28c215747
jbferraz <aluno@GRVL301B-01>
2019-06-28 21:47:57 -0300

    view e model do alunos

---
+++ application/views/alunosCadastro.php
@@ -0,0 +1,22 @@
+<div class="row">
+        <div class="col-lg-3 col-md-3 col-3"></div>
+        <div class="col-lg-6 col-md-6 col-6">
+                <form method="post" class="form-group">
+                        <div class="form-group">
+                            <input name = "emp_id"  class="form-control" type = "text" id = "emp_id" size="3" maxlength="2" placeholder="    ID">
+                        </div>
+                            <input name = "delete" type = "submit" id = "delete" value = "Deletar" class="btn btn-danger"> <p>
+                        <div class="form-group">
+                            <input name="up_id" class="form-control" type="text" id = "up_id" size="2" maxlength="2" placeholder="    ID">
+                        </div>
+                            <input name = "update" type = "submit" id = "update" value = "Atualizar" class="btn btn-warning">  <p>
+                        <div class="form-group">	
+                            <input type="text" class="form-control" name="nome" placeholder="Digite o nome ..." >
+                        </div>
+                        <input name="pesquisar" type="submit" value="Pesquisar" class="btn btn-primary"> <p></p>
+
+                        <input name="todos" type="submit" value="Listar Todos" class="btn btn-success"> <p></p>
+                </form>
+        </div>
+        <div class="col-lg-3 col-md-3 col-3"></div>
+</div>
