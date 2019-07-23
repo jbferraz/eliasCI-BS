3469c4636c67c335ab493a34447720ec9f5b3e81
jbferraz <>
2019-07-22 21:18:59 -0300

    ajuste para publicar online 2

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'http://eliascibs2019-com-br.umbler.net';
-$config['base_url'] = 'http://localhost/eliasCI+BS';
+$config['base_url'] = 'http://eliascibs2019-com-br.umbler.net/';
+//$config['base_url'] = 'http://localhost/eliasCI+BS/';
 
 /*
 |--------------------------------------------------------------------------
